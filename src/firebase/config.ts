5f8fb3fe065c9371bf84f6bd0279b20432c1c1cd
Wahaj Mansoor <wahajmansoor7@gmail.com>
2025-11-26 16:05:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-164991682-25704",
+  "appId": "1:1080418225784:web:33300eb970c2b5a674678d",
+  "apiKey": "AIzaSyDyyFXYzqWOp6VbZskjks1FHu_wE5y22j0",
+  "authDomain": "studio-164991682-25704.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1080418225784"
+};
